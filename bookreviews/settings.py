0df0203e394393d551d3b5d77ceacaf5028fb9bf
Camila <yourname@example.com>
2024-10-15 23:27:09 +0100

    Refactor: Update DEBUG setting to False and ALLOWED_HOSTS in settings.py

--- bookreviews/settings.py
+++ bookreviews/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security Configuration
 SECRET_KEY = config('SECRET_KEY') 
-DEBUG = True 
+DEBUG = False 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'bookview-1fe2bb34917a.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = [
